af48cfa8715f1d004f299c64e49e2834d7cd10b4
EC2 Default User <ec2-user@ip-10-0-1-239.ap-northeast-2.compute.internal>
2023-07-16 14:25:41 +0000

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.exceptions.JedisConnectionException;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    private final Jedis jedis;
+
+    public DemoApplication() {
+        // Replace "redis-cluster-endpoint" with the endpoint of your Elasticache Redis cluster
+        jedis = new Jedis("demo-redis.yww855.ng.0001.apn2.cache.amazonaws.com");
+    }
+
+    @GetMapping("/")
+    public String getData() {
+        String value = jedis.get("myKey");
+        return "Retrieved value from Redis: " + value;
+    }
+
+    @PostMapping("/")
+    public String setData(@RequestBody String data) {
+        jedis.set("myKey", data);
+        return "Data stored in Redis successfully!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+}
